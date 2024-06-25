7a457aaa4aaf9029f23f0d0011168e4bc401b222
yeinP <yenny_1122@naver.com>
2024-06-25 13:04:41 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{padding:"40px", margin: "0 auto"}}>
+      <div style={{border: "1px solid red", width:"400px", textAlgin:"center"}}>
+        <span style={{display:"block", padding:"10px 0"}}>Are you sure?</span>
+        <button type="button">cancle</button>
+        <button type="button">ok</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
